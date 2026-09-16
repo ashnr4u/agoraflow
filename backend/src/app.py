aa5449341bc64a504719e402c257a3cd136abe3b
Ashutosh <ashurav786@gmail.com>
2026-09-16 20:28:37 +0530

    chore: set up database models and PostgreSQL connection

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,1 +1,15 @@
-print("welcome")
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from models import Base
+
+load_dotenv() # reads variables from a .env file and sets them in os.environ
+
+user= os.getenv("DB_USER")
+password= os.getenv("DB_password")
+
+print("Debugging --------------------------------------")
+db= create_engine(f"postgresql://{user}:{password}@localhost:5000/agoraflow")
+
+# creates all ORM-defined tables in the database
+Base.metadata.create_all(db)
---
+++ backend/src/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    user_email: Mapped[str]  = mapped_column(unique=True)
+    name: Mapped[str]
+    hashed_password: Mapped[str]
+    role: Mapped[str]
+    created_at: Mapped[datetime]
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    event_id: Mapped[int] = mapped_column(primary_key=True)
+    event_name: Mapped[str] = mapped_column(unique=True)
+    max_capacity: Mapped[int]
+    registration_start: Mapped[datetime]
+    registration_close: Mapped[datetime]
+    event_start_date: Mapped[datetime]
+    description: Mapped[str]
+    organizer_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
+
+
+class Registration(Base):
+    __tablename__ = "registrations"
+
+    registration_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
+    event_id: Mapped[int] = mapped_column(ForeignKey("events.event_id"))
+    registered_at: Mapped[datetime]
