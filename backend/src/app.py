65711de9713013aabbead81c874b3a9edc231a2d
Ashutosh <ashurav786@gmail.com>
2026-09-18 00:39:35 +0530

    feat: verify FastAPI PostgreSQL connection

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,15 +1,34 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 from models import Base
+from fastapi import FastAPI, Depends
 
 load_dotenv() # reads variables from a .env file and sets them in os.environ
 
 user= os.getenv("DB_USER")
-password= os.getenv("DB_password")
+password= os.getenv("DB_PASSWORD")
 
 print("Debugging --------------------------------------")
-db= create_engine(f"postgresql://{user}:{password}@localhost:5000/agoraflow")
+db_engine = create_engine(f"postgresql://{user}:{password}@localhost:5000/agoraflow")
 
 # creates all ORM-defined tables in the database
-Base.metadata.create_all(db)
+# Base.metadata.create_all(db_engine)
+app= FastAPI()
+
+my_session = sessionmaker(bind = db_engine)
+def get_db():
+    #my_session() creates a Session object through which I can perform database operations.
+    db=my_session()
+    try: 
+        yield db
+
+    finally:
+        db.close()
+
+@app.get("/")
+def test(test_db = Depends(get_db)):
+    result = test_db.execute(text("Select 1"))
+    value= result.scalar()
+    return {"result":value}
